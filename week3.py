d6dfa1eae689120f86d8e6bc19678e0b2a79c599
John .W. Taylor-Kamara <johntaylorkamara05@gmail.com>
2026-05-01 17:48:05 +0000

    Official submission for assignment

---
+++ week3.py
@@ -0,0 +1,24 @@
+print("hello world")
+full_name = "John Taylor-Kamara"
+id = 905005639
+semester = 2
+cgpa = 2.0
+is_active = False
+print(full_name)
+print(id)
+print(semester)
+print(cgpa)
+print(is_active)
+print(f"full name: {full_name}")
+print(f"id: {id}")
+print(f"semester: {semester}")
+print(f"cgpa: {cgpa}")
+# collecting input for string, int. and float
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height: "))
+
+print(f"name: {name}")
+print(f"age: {age}")
+print(f"your height is {height} cm")
